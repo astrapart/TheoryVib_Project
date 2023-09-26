e5eacc0686d65fd315e0e72760b4470b2827e9a9
astrapart <arnaud.strapart@student.uliege.be>
2023-09-26 14:28:18 +0200

    Update Code Python Strucutre.py
    ceci est un test _ Arnaud Github

--- Code Python Strucutre.py
+++ Code Python Strucutre.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+#ceci est un test github
+
 # Créez une figure 3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
